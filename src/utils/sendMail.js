507b2d81b30949edec12c04a59c29e2331700b62
AnirbanRoyy <anirban15987@gmail.com>
2024-12-08 20:51:29 +0530

    add sendMail utility

---
+++ src/utils/sendMail.js
@@ -0,0 +1,32 @@
+import nodemailer from "nodemailer";
+
+const sendEmail = async (to, subject, message) => {
+    try {
+        // Create a transporter
+        const transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+                user: process.env.EMAIL_USERNAME,
+                pass: process.env.EMAIL_PASSWORD,
+            },
+        });
+
+        // Define the email options
+        const mailOptions = {
+            from: process.env.EMAIL_USERNAME, // Sender address
+            to, // Receiver email address
+            subject, // Subject line
+            text: message, // Plain text message
+        };
+
+        // Send the email
+        const info = await transporter.sendMail(mailOptions);
+        console.log("Email sent: ", info.response);
+        return info;
+    } catch (error) {
+        console.log("Error sending email -> ", error);
+        throw new Error("Email sending failed");
+    }
+};
+
+export default sendEmail;
